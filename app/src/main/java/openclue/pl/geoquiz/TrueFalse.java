80a6e9aa4b31ce48327e02735d045c064f102495
Daniel Jablonski <d.jablonski@openclue.pl>
2015-03-15 19:15:30 +0100

    Finished chapter 2

---
+++ app/src/main/java/openclue/pl/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package openclue.pl.geoquiz;
+
+/**
+ * Created by Daniel on 2015-03-15.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int mQuestion, boolean mTrueQuestion) {
+        this.mQuestion = mQuestion;
+        this.mTrueQuestion = mTrueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
